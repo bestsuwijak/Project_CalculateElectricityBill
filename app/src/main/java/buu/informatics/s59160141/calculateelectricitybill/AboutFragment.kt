9baf20435ef86de99eeb3530e4cba86bcf3eab60
bestsuwijak <42570047+bestsuwijak@users.noreply.github.com>
2019-10-31 01:13:36 +0700

    Add share

--- app/src/main/java/buu/informatics/s59160141/calculateelectricitybill/AboutFragment.kt
+++ app/src/main/java/buu/informatics/s59160141/calculateelectricitybill/AboutFragment.kt
@@ -1,11 +1,10 @@
 package buu.informatics.s59160141.calculateelectricitybill
 
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import buu.informatics.s59160141.calculateelectricitybill.databinding.FragmentAboutBinding
 
@@ -22,7 +21,35 @@ class AboutFragment : Fragment() {
         val binding = DataBindingUtil.inflate<FragmentAboutBinding>(
             inflater,R.layout.fragment_about,container,false
         )
+
+        setHasOptionsMenu(true)
         return inflater.inflate(R.layout.fragment_about, container, false)
+    }
+
+    //Share
+    private fun getShareIntent() : Intent {
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.setType("text/plain")
+            .putExtra(Intent.EXTRA_TEXT, getString(R.string.share_success_text))
+        return shareIntent
+    }
+    private fun shareSuccess() {
+        startActivity(getShareIntent())
+    }
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.share_menu, menu)
+        // check if the activity resolves
+        if (null == getShareIntent().resolveActivity(activity!!.packageManager)) {
+            // hide the menu item if it doesn't resolve
+            menu.findItem(R.id.share)?.setVisible(false)
+        }
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.share -> shareSuccess()
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 
