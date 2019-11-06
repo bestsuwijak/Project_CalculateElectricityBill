35b69eb4cc82efb8769ef264d1da14641e37c6f9
bestsuwijak <42570047+bestsuwijak@users.noreply.github.com>
2019-11-06 19:04:50 +0700

    Add safe args

--- app/src/main/java/buu/informatics/s59160141/calculateelectricitybill/CalculateFragment.kt
+++ app/src/main/java/buu/informatics/s59160141/calculateelectricitybill/CalculateFragment.kt
@@ -29,15 +29,12 @@ class CalculateFragment : Fragment() {
             inflater, R.layout.fragment_calculate, container, false
         )
 
-        binding.btnDetail.setOnClickListener { view: View ->
-            view.findNavController().navigate(R.id.action_calculateFragment_to_detailFragment)
-        }
-
         binding.apply {
             resultText.visibility = View.GONE
             textView6.visibility = View.GONE
             btnDetail.visibility = View.GONE
         }
+
         binding.btnCalculate.setOnClickListener {
             //Toast.makeText(context,"ราคา = " +R.id.input,Toast.LENGTH_LONG).show()
             //println(binding.input.text)
@@ -51,37 +48,57 @@ class CalculateFragment : Fragment() {
             }
         }
 
+        binding.btnDetail.setOnClickListener { view: View ->
+            view.findNavController()
+                .navigate(CalculateFragmentDirections
+                    .actionCalculateFragmentToDetailFragment(eb.unit, eb.res150, eb.res250,
+                        eb.res400, eb.serviceCharge, eb.ft, eb.vat, eb.sum))
+//            view.findNavController()
+//                .navigate(R.id.action_calculateFragment_to_detailFragment)
+    }
+
         return binding.root
     }
 
 
 
     var result = String.format("%1.2f",0.00).toFloat()
+    var sumRes: Float = 0F
     private fun calculate(input: Int) {
-        val unit = input
+        var unit = input
+        result = 0F
         eb.unit = unit.toString()
         if (unit == 0) {
             //snackbar show
         }else {
 
-            if(unit > 0 && unit <= 150){
-                result = String.format("%1.2f", unit * 3.2484).toFloat()
-                eb.res150 = result.toString()
+            if(unit > 400){
+                sumRes = String.format("%1.2f", (unit - 400) * 4.4217).toFloat()
+                result += sumRes
+                eb.res400 = sumRes.toString()
+                unit -= unit - 400
+
             }
             if(unit > 150 && unit <= 400){
-                result = String.format("%1.2f", (150 * 3.2484) + ((unit - 150) * 4.2218)).toFloat()
-                eb.res250 = result.toString()
+                sumRes = String.format("%1.2f", (unit - 150) * 4.2218).toFloat()
+                result += sumRes
+                eb.res250 = sumRes.toString()
+                unit -= unit - 150
+
             }
-            if(unit > 400){
-                result = String.format("%1.2f", (150 * 3.2484) + (250 * 4.2218) + ((unit-400) * 4.4217)).toFloat()
-                eb.res400 = result.toString()
+            if(unit > 0 && unit <= 150){
+                sumRes = String.format("%1.2f", unit * 3.2484).toFloat()
+                result += sumRes
+                eb.res150 = sumRes.toString()
+                unit -= unit
+
             }
 
 
             result += String.format("%1.2f", 38.22).toFloat()  //+ค่าบริการ
             eb.serviceCharge = String.format("%1.2f", 38.22)
             //println("ค่าไฟฟ้าฐาน = " +result)
-            val Ft = String.format("%1.2f", (unit * (-11.60))/100).toFloat()
+            val Ft = String.format("%1.2f", (input * (-11.60))/100).toFloat()
             eb.ft = Ft.toString()
             //println("ค่าไฟฟ้าผันแปร = " +Ft)
             val vat = String.format("%1.2f", ((result + Ft)*0.07)).toFloat()
@@ -95,11 +112,13 @@ class CalculateFragment : Fragment() {
 
 
             //Toast.makeText(context,"ราคา = " + (String.format("%1.2f",result).toFloat()), Toast.LENGTH_LONG).show()
-            Snackbar.make(requireView(),"ราคา  =  " + (String.format("%1.2f",result).toFloat()), Snackbar.LENGTH_SHORT).show();
+            Snackbar.make(requireView(),"ราคา  =  " + (String.format("%1.2f",result).toFloat()) + " บาท", Snackbar.LENGTH_SHORT).show();
             binding.invalidateAll()
         }
     }
 
 
 }
+
+
 
--- app/src/main/java/buu/informatics/s59160141/calculateelectricitybill/DetailFragment.kt
+++ app/src/main/java/buu/informatics/s59160141/calculateelectricitybill/DetailFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import buu.informatics.s59160141.calculateelectricitybill.databinding.FragmentDetailBinding
 import kotlinx.android.synthetic.main.fragment_detail.*
@@ -17,6 +18,8 @@ class DetailFragment : Fragment() {
 
     private lateinit var binding: FragmentDetailBinding
     private var eb:ElectricBill = ElectricBill()
+    lateinit var args: DetailFragmentArgs
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -26,10 +29,24 @@ class DetailFragment : Fragment() {
             inflater,R.layout.fragment_detail,container,false
         )
 
+        args = DetailFragmentArgs.fromBundle(arguments!!)
         binding.electricBill = eb
+        saveBinding()
         binding.invalidateAll()
 
         return binding.root
+    }
+
+    private  fun saveBinding() {
+        eb.unit = args.unit
+        eb.res150 = args.unit150
+        eb.res250 = args.unit250
+        eb.res400 = args.unit400
+        eb.serviceCharge = args.serviceCharge
+        binding.baseElectricChargeText.text = String.format("%1.2f", eb.res150.toFloat() + eb.res250.toFloat() + eb.res400.toFloat() + eb.serviceCharge.toFloat())
+        eb.ft = args.ft
+        eb.vat = args.vat
+        eb.sum = args.sum
     }
 
 
